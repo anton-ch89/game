f6217a30e6dc2260ad285b0b4760d1b236e6df10
Anton Chudinov <anton.ch89@gmail.com>
2021-06-04 16:53:50 +0500

    Доработанная версия

--- script.js
+++ script.js
@@ -5,46 +5,52 @@ let isNumber = function (n) {
 };
 
 const getNum = function () {
-    const randNum = 10; /* Math.round(Math.random() * 100);*/
+    const randNum = Math.round(Math.random() * 100);
     let userNum = prompt('Угадай число от 0 до 100');
-    let attemptNum = 10;
-     
-  
-        if (attemptNum > 0) {
-                    
-        if (userNum === null) {
-            return alert('Игра окончена');
-        } else if (+userNum === randNum) {
-            confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-            if (true) {
+    let attemptNum = 9
+        ;
+
+    const guessNum = function () {
+        if (+userNum === randNum) {
+            let answerWin = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+            if (answerWin) {
                 return getNum();
             } else {
                 return alert('Игра окончена');
             }
-        } else {
-            const guessNum = function () {
-                if (!isNumber(userNum)) {
-                    userNum = prompt('Ведите число!');
-                    return guessNum();
-                } else if (randNum > userNum) {
-                    userNum = prompt(`Загаданное число больше, осталось ${attemptNum--} попыток `);
-                    return guessNum();
-                } else if (randNum < userNum) {
-                    userNum = prompt(`Загаданное число меньше, осталось ${attemptNum--} попыток `);
-                    return guessNum();
-                } 
-            };
-            guessNum();
+        } else if (userNum === null) {
+            return alert('Игра окончена');
+        } else if (!isNumber(userNum)) {
+            userNum = prompt('Ведите число!');
+            return guessNum();
+        } else if (randNum > userNum) {
+            userNum = prompt(`Загаданное число больше, осталось ${attemptNum--} попыток.`);
+            if (attemptNum > 0) {
+                return guessNum();
+            } else {
+                let answerLoose = confirm('Попытки закончились, хотите сыграть еще?');
+                if (answerLoose) {
+                    return getNum();
+                } else {
+                    return alert('Игра окончена');
+                }
+            }
+        } else if (randNum < userNum) {
+            userNum = prompt(`Загаданное число меньше, осталось ${attemptNum--} попыток.`);
+            if (attemptNum > 0) {
+                return guessNum();
+            } else {
+                let answerLoose = confirm('Попытки закончились, хотите сыграть еще?');
+                if (answerLoose) {
+                    return getNum();
+                } else {
+                    return alert('Игра окончена');
+                }
+            }
+            return guessNum();
         }
 
-        } else {
-            confirm ('Попытки закончились, хотите сыграть еще?');
-            if (true) {
-                return getNum();
-            } else {
-                alert ('Игра окончена');
-            }
-        }
-  
+    };
+    guessNum();
 };
 getNum();
